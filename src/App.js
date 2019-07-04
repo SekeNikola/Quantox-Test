21fc4e9c883274ba1a8ed004d81472375d38750f
Seke Nikola <sekenikola@gmail.com>
2019-07-04 12:55:38 +0200

    add Router

--- src/App.js
+++ src/App.js
@@ -1,71 +1,119 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios'
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Preloader, Placeholder } from "react-preloading-screen";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import CryptoDetails from "./CryptoDetails";
+import "./App.css";
 
-function App() {
+const App = () => {
   const [data, setData] = useState("");
-  const [coinValue, setCoinValue] = useState("")
-  let [value, setValue] = useState("")
+  const [coinValue, setCoinValue] = useState("");
 
   useEffect(() => {
-  let url = "https://cors-anywhere.herokuapp.com/https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
-
+    let url =
+      "https://cors-anywhere.herokuapp.com/https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
     axios
-      .get(url,{
+      .get(url, {
         headers: {
-          'X-CMC_PRO_API_KEY': 'c8643baa-31bf-4d31-8868-af73ce84766b'
-        },
+          "X-CMC_PRO_API_KEY": "c8643baa-31bf-4d31-8868-af73ce84766b"
+        }
       })
       .then(result => setData(result.data.data));
   }, []);
 
-
-
-
   return (
-    <div id="potd" className="container text-center mb-5 mt-5">
-      <table className="table ">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Short Name</th>
-            <th scope="col">$ Value</th>
-            <th scope="col">Last 24h</th>
-            <th scope="col">Amount you own</th>
-            <th scope="col">$ value of your coins</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Array.isArray(data) &&
-            data.map(objects => {
-              let test = objects.quote.USD.price.toFixed(2)
-              let Foo = (function() {
-                return test * coinValue
-            });
+    <>
+      <Router>
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={() => {
               return (
-                <tr key={objects.name}>
-                  <td>{objects.name}</td>
-                  <td>
-                    {objects.symbol}
-                  </td>
-                  <td onLoad={()=>setValue(this.test)}>
-                    {test}
-                  </td>
-                  <td style={{color: objects.quote.USD.percent_change_24h > 0 ? "green" : "red"}}>{objects.quote.USD.percent_change_24h.toFixed(2)}</td>
-                  <td>
-                    <input id="input" required type="number" onChange={e => setCoinValue(e.target.value)} /> <br/>
-                    <button className="btn" disabled={!coinValue} type="submit" onClick={Foo}>Submit</button>
-                  </td>
-                  <td>
-                    <Foo></Foo>
-                  </td>
-                </tr>
+                <Preloader>
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>Short Name</th>
+                        <th>$ Value</th>
+                        <th>Last 24h</th>
+                        <th>Amount you own</th>
+                        <th>$ value of your coins</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {Array.isArray(data) &&
+                        data.map(objects => {
+                          let price = objects.quote.USD.price.toFixed(2);
+                          const result = () => {
+                            console.log(price * coinValue);
+                            return <p>{(price * coinValue).toFixed(2)}</p>;
+                          };
+
+                          return (
+                            <tr key={objects.id}>
+                              <td>
+                                <Link
+                                  target="_blank"
+                                  to={`/details/${objects.name}`}
+                                >
+                                  {objects.name}
+                                </Link>
+                              </td>
+
+                              <td>{objects.symbol}</td>
+                              <td>{price}</td>
+                              <td
+                                style={{
+                                  color:
+                                    objects.quote.USD.percent_change_24h > 0
+                                      ? "green"
+                                      : "red"
+                                }}
+                              >
+                                {objects.quote.USD.percent_change_24h.toFixed(
+                                  2
+                                )}
+                              </td>
+                              <td>
+                                <input
+                                  required
+                                  type="number"
+                                  pattern="^-?[0-9]\d*\.?\d*$"
+                                  onChange={e => {
+                                    setCoinValue(e.target.value);
+                                  }}
+                                />{" "}
+                                <br />
+                                <button
+                                  className="btn"
+                                  disabled={!coinValue}
+                                  type="submit"
+                                  onClick={result}
+                                >
+                                  Submit
+                                </button>
+                              </td>
+                              <td>{result()}</td>
+                            </tr>
+                          );
+                        })}
+                    </tbody>
+                  </table>
+                  <Placeholder>
+                    <span>Loading...</span>
+                  </Placeholder>
+                </Preloader>
               );
-            })}
-        </tbody>
-      </table>
-      </div>
+            }}
+          />
+
+          <Route exact={true} path="/details/:name" component={CryptoDetails} />
+        </Switch>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/CryptoDetails.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CryptoDetails = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <div>
+        <p>Testing</p>
+      </div>
+    </>
+  );
+};
+
+export default CryptoDetails;
